378005d9c03af9cd163c71a66ea4611ebe2f9b67
Sprazzeus <sprazzeus@gmail.com>
2016-06-30 14:47:06 +0800

    fixed broken nested routes

--- db/migrate/20160629035426_devise_create_users.rb
+++ db/migrate/20160629035426_devise_create_users.rb
@@ -36,6 +36,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :name,                 unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
