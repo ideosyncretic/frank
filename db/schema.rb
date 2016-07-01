316cee70040c7389dfca312a2bc3d63800b40a66
Sprazzeus <sprazzeus@gmail.com>
2016-07-01 08:32:58 +0800

    removed extra migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160630050331) do
+ActiveRecord::Schema.define(version: 20160630175917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160630050331) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
