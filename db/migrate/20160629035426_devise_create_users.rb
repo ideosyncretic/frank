e65ae629c334d844feff744f436f5f6e81f70965
Sprazzeus <sprazzeus@gmail.com>
2016-07-02 16:07:45 +0800

    remove name index from first migration file

--- db/migrate/20160629035426_devise_create_users.rb
+++ db/migrate/20160629035426_devise_create_users.rb
@@ -36,7 +36,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    add_index :users, :name,                 unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
