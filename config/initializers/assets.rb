8879e91a4d8d68d661a918b392588fe3a6dc6127
Sprazzeus <sprazzeus@gmail.com>
2016-07-02 17:00:12 +0800

    updated post styling

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( post_limit_countdown.js )
+Rails.application.config.assets.precompile += %w( post_limit_countdown.js clickable-row.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
